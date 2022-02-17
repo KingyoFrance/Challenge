326159c555a8d1553fc21654842b0824ce5b3531
KingyoFrance <99845989+KingyoFrance@users.noreply.github.com>
2022-02-17 21:24:59 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-    $servername='localhost';
-    $username='root';
-    $password='';
-    $dbname = "my_db";
+    $servername='sql11.freemysqlhosting.net';
+    $username='sql11473567';
+    $password='XjUX4CL5zR';
+    $dbname = "sql11473567";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
